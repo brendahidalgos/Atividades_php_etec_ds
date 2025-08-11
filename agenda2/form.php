eb0ccfb02a7ca166199bb73f215fb8f300b6a3f2
Brenda Hidalgo <brendascarante99@gmail.com>
2025-08-11 17:34:53 -0300

    upload agenda 2 teste

---
+++ agenda2/form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda 2 DS</title>
+</head>
+<body>
+    <form id="form1" name="form1" method="POST" action="acao.php">
+        <p>Nome <input type="text" name="nome" id="idnome" placeholder="Digite seu nome"></p>
+        <p>Idade <input type="text" name="idade" id="ididade" placeholder="Ex: 18"></p>
+        <input type="submit" value="enviar" name="Enviar" id="idenviar">
+    </form>
+</body>
+</html>
