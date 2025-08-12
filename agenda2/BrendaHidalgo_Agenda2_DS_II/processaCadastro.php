f4afb57e8820492bec4d00693873b37c6c8abe04
Brenda Hidalgo <brendascarante99@gmail.com>
2025-08-12 08:53:19 -0300

    up basic

--- agenda2/BrendaHidalgo_Agenda2_DS_II/processaCadastro.php
+++ agenda2/BrendaHidalgo_Agenda2_DS_II/processaCadastro.php
@@ -6,18 +6,18 @@
     <title>Processa Cadastro</title>
     <link rel="stylesheet" href="estilos/style.css">
 </head>
-<body>
-    <div id="idform">
-        <?php
-        echo "Seu nome é: ".$_POST['nome']."<br>";
-        echo "Sua profissão é: ".$_POST['profissao']."<br>";
-        echo "Pretensão Salárial: R$".$_POST['salario']."<br>";
-        echo "Experiências anteriores: ".$_POST['experiencia']."<br>";
-        ?>      
-    </div>  
-    <?php   
-    echo "<br>";
-    echo "A dificuldade no desenvolvimento do código foi no arquivo processaCadastro.php em que a organização (sequência) é fundamental para não dar erro, principalmente na concatenação.   e a escolha de Qual foi o maior desafio ao desenvolver o código. Como decidiu organizar os dados no formulário.";
-    ?>
+<body>    
+    <section class="resection">
+        <div class="reform">
+            <?php
+            echo "Seu nome é: ".$_POST['nome']."<br>";
+            echo "Sua profissão é: ".$_POST['profissao']."<br>";
+            echo "Pretensão Salárial: R$".$_POST['salario']."<br>";
+            echo "Experiências anteriores: ".$_POST['experiencia']."<br>";
+            echo "<br>";
+            echo "A dificuldade no desenvolvimento do código foi no arquivo processaCadastro.php em que a organização (sequência) é fundamental para não dar erro, principalmente na concatenação.   e a escolha de Qual foi o maior desafio ao desenvolver o código. Como decidiu organizar os dados no formulário.";
+            ?>
+        </div>
+    </section>   
 </body>
 </html>
