fb8695dd82763c7faa1133c3a5b3dfaaf73bfd22
Brenda Hidalgo <brendascarante99@gmail.com>
2025-08-12 11:00:30 -0300

    up

--- agenda2/BrendaHidalgo_Agenda2_DS_II/processaCadastro.php
+++ agenda2/BrendaHidalgo_Agenda2_DS_II/processaCadastro.php
@@ -11,17 +11,22 @@
     <section class="resection">        
         <div class="reform">
             <p class="rep">
-                <?php echo "Seu nome é: ".$_POST['nome']."<br>";            
+                <?php
+                $experiencia = trim($_POST['experiencia']);
+                if (empty($experiencia)) {
+                    $experiencia = "Sem experiência!";
+                }
+                echo "Seu nome é: ".$_POST['nome']."<br>";            
                 echo "Sua profissão é: ".$_POST['profissao']."<br>";
                 echo "Pretensão Salarial: R$".$_POST['salario']."<br>";
-                echo "Experiências anteriores: ".$_POST['experiencia']."<br>";
+                echo "Experiências anteriores: " .($experiencia);
                 echo "<br>";?>
             </p>
             <h2>Dificuldades no Exercício Proposto:</h2>
             <ul class="reul">
-                <li>Frameworks - desenvolvo a maioria das vezes em css puro, não consegui ainda ter prática em lembrar os códigos do W3schools.</li>
-                <li>JavaScript - tudo (tive que pesquisar inclusive),como eu mostro uma frase caso a pessoa não escreva nada em "experiências anteriores".</li>
-                <li>PHP - somente na concatenação e na organização do código, por misturar html e php.</li>
+                <li>Frameworks - desenvolvo a maioria das vezes em css puro, não consegui ainda ter prática em lembrar os códigos do W3schools.</li>                
+                <li>PHP - Primeiro na concatenação e na organização do código, por misturar html e php. Segundo, como eu mostro uma frase caso a pessoa não escreva nada em "experiências anteriores".</li>
+                <li>JavaScript - tudo, inclusive só pesquisando.</li>
             </ul>            
             <br>
             <input type="button" value="Voltar" name="voltar" id="voltar" onclick="history.back()">
