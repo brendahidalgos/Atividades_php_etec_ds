f0ba10800afe33ca1629a2ebcb7d8fb0e79f17b5
Brenda Hidalgo <brendascarante99@gmail.com>
2025-08-11 21:22:18 -0300

    up

--- agenda2/BrendaHidalgo_Agenda2_DS_II/processaCadastro.php
+++ agenda2/BrendaHidalgo_Agenda2_DS_II/processaCadastro.php
@@ -1,4 +1,8 @@
 <?php
-echo "Nome: $_POST['nome']";
-echo "O comentário reflexivo final devem estar dentro do processaCadastro.php, como texto ou echo.";
+echo "Seu nome é: ".$_POST['nome']."<br>";
+echo "Sua profissão é: ".$_POST['profissao']."<br>";
+echo "Pretensão Salárial: ".$_POST['salario']."<br>";
+echo "Experiência anteriores: ".$_POST['experiencia']."<br>";
+
+echo "A dificuldade no desenvolvimento do código foi no arquivo processaCadastro.php em que a organização (sequência) é fundamental para não dar erro, principalmente na concatenação.   e a escolha de Qual foi o maior desafio ao desenvolver o código. Como decidiu organizar os dados no formulário.-->";
 ?>
