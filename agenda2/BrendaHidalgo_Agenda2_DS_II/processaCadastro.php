41ccb5533ec776e97fbff07fef47c8d6a3b0ee06
Brenda Hidalgo <brendascarante99@gmail.com>
2025-08-12 21:01:57 -0300

    up

--- agenda2/BrendaHidalgo_Agenda2_DS_II/processaCadastro.php
+++ agenda2/BrendaHidalgo_Agenda2_DS_II/processaCadastro.php
@@ -12,24 +12,40 @@
         <div class="reform">
             <p class="rep">
                 <?php
+                // Verifica se o campo de texto foi preenchido, senão usa o radio
+                if (!empty($_POST['sexo_texto'])) {
+                    $sexo = $_POST['sexo_texto'];
+                } else {
+                    $sexo = $_POST['sexo'];
+                }
                 $experiencia = trim($_POST['experiencia']);
                 if (empty($experiencia)) {
                     $experiencia = "Sem experiência!";
                 }
-                echo "Seu nome é: ".$_POST['nome']."<br>";            
-                echo "Sua profissão é: ".$_POST['profissao']."<br>";
+                echo "Seu nome é: ".$_POST['nome']."<br>";   
+                echo "Sexo: ".$sexo."<br>";
+                echo "E-mail: ".$_POST['email']."<br>";
+                echo "Profissão: ".$_POST['profissao']."<br>";
                 echo "Pretensão Salarial: R$".$_POST['salario']."<br>";
                 echo "Experiências anteriores: " .($experiencia);
                 echo "<br>";?>
             </p>
-            <h2>Dificuldades no Exercício Proposto:</h2>
+            <h2>Desafios no Exercício Proposto:</h2>
             <ul class="reul">
-                <li>Frameworks - desenvolvo a maioria das vezes em css puro, não consegui ainda ter prática em lembrar os códigos do W3schools.</li>                
-                <li>PHP - Primeiro na concatenação e na organização do código, por misturar html e php. Segundo, como eu mostro uma frase caso a pessoa não escreva nada em "experiências anteriores".</li>
-                <li>JavaScript - tudo, inclusive só pesquisando.</li>
+                <p class="rep">PHP</p>
+                <li>Concatenação e na organização do código, por misturar html e php.</li>
+                <li>Como verificar se no campo "sexo" tem marcado um "radio" ou tem algo escrito no campo "text".</li>
+                <li>Como mostrar uma frase automática com "Sem experiência", caso a pessoa não escreva nada em "experiências anteriores".</li>
+                <br>
+                <p class="rep">Frameworks</p>
+                <li>Frameworks - desenvolvo a maioria das vezes em css puro, não consegui ainda ter prática com os códigos do W3schools.</li>
+                <br>
+                <p class="rep">JavaScript</p>
+                <li>Não tenho prática, tive que pesquisar no endereço: <a href="https://www.google.com/search?q=quero+que+no+html+quando+eu+clicar+no+input+text+ele+tira+a+sele%C3%A7%C3%A3o+do+radio+com+javascript&oq=quero+que+no+html+quando+eu+clicar+no+input+text+ele+tira+a+sele%C3%A7%C3%A3o+do+radio+com+javascript&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCDQ5NDBqMGoxqAIAsAIA&sourceid=chrome&ie=UTF-8" target="_blank">clique aqui</a>, para tirar a seleção do campo "radio" caso o campo "text" tenha algo escrito.</li>
             </ul>            
             <br>
-            <p>Qual foi o maior desafio ao desenvolver o código.Como decidiu organizar os dados no formulário.</p>
+            <p class="rep">Como decidiu organizar os dados no formulário.</p>
+            <p style="text-align:left;">Eu pensei em como uma pessoa faria esse cadastro e quais os dados principais um recrutador precisaria. Porém para mim faltou Estado e Cidade se fosse uma empresa nacional e não regional.</p>
             <input type="button" value="Voltar" name="voltar" id="voltar" onclick="history.back()">
         </div>
     </section>   
