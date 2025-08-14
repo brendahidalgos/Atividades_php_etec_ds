b9288542e64f7cd1bad712e02f080595a87d10dc
Brenda Hidalgo <brendascarante99@gmail.com>
2025-08-14 00:20:01 -0300

    up php

--- agenda2/BrendaHidalgo_Agenda2_DS_II/processaCadastro.php
+++ agenda2/BrendaHidalgo_Agenda2_DS_II/processaCadastro.php
@@ -38,14 +38,14 @@
                 <li>Como mostrar uma frase automática com "Sem experiência", caso a pessoa não escreva nada em "experiências anteriores".</li>
                 <br>
                 <p class="rep">Frameworks</p>
-                <li>Frameworks - desenvolvo a maioria das vezes em css puro, não consegui ainda ter prática com os códigos do W3schools.</li>
+                <li>Frameworks - decidi não usar frameworks porque estudei durante as férias CSS puro, e resolvi por em prática (gostei bastante).</li>
                 <br>
                 <p class="rep">JavaScript</p>
                 <li>Não tenho prática, tive que pesquisar no endereço: <a href="https://www.google.com/search?q=quero+que+no+html+quando+eu+clicar+no+input+text+ele+tira+a+sele%C3%A7%C3%A3o+do+radio+com+javascript&oq=quero+que+no+html+quando+eu+clicar+no+input+text+ele+tira+a+sele%C3%A7%C3%A3o+do+radio+com+javascript&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCDQ5NDBqMGoxqAIAsAIA&sourceid=chrome&ie=UTF-8" target="_blank">clique aqui</a>, para tirar a seleção do campo "radio" caso o campo "text" tenha algo escrito.</li>
             </ul>            
             <br>
-            <p class="rep">Como decidiu organizar os dados no formulário.</p>
-            <p style="text-align:left;">Eu pensei em como uma pessoa faria esse cadastro e quais os dados principais um recrutador precisaria. Porém para mim faltou Estado e Cidade se fosse uma empresa nacional e não regional.</p>
+            <h2>Organização dos dados no formulário:</h2>
+            <p style="text-align:left;">Eu pensei em como uma pessoa faria esse cadastro e quais os dados principais um recrutador precisaria. Porém, acho que faltou Estado e Cidade se fosse uma empresa nacional e não regional.</p>
             <input type="button" value="Voltar" name="voltar" id="voltar" onclick="history.back()">
         </div>
     </section>   
