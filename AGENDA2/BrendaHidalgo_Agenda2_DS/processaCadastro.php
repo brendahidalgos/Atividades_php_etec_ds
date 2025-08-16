71edb863ae4b371ef2567a07b0358bc74f6298e9
Brenda Hidalgo <brendascarante99@gmail.com>
2025-08-15 21:58:16 -0300

    elaborei o mesmo site com framework w3scholls

---
+++ AGENDA2/BrendaHidalgo_Agenda2_DS/processaCadastro.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Processa Cadastro</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+<body class="w3-pale-blue">
+    <div class="w3-container w3-center w3-margin-top">
+        <h1 class="w3-text-black" style="text-shadow:1px 1px 0 #493838bb">Cadastro Realizado!</h1>
+    </div>
+    <div class="w3-card-4 w3-white w3-margin-top w3-margin-bottom w3-padding w3-container w3-content w3-round-large" style="max-width:700px;">
+        <div class="w3-container">
+            <p>
+                <?php
+                // Verifica se o campo de texto foi preenchido, senão usa o radio
+                if (!empty($_POST['sexo_texto'])) {
+                    $sexo = $_POST['sexo_texto'];
+                } else {
+                    $sexo = $_POST['sexo'];
+                }
+                $experiencia = trim($_POST['experiencia']);
+                if (empty($experiencia)) {
+                    $experiencia = "Sem experiência!";
+                }
+                echo "Seu nome é: ".$_POST['nome']."<br>";
+                echo "Sexo: ".$sexo."<br>";
+                echo "E-mail: ".$_POST['email']."<br>";
+                echo "Profissão: ".$_POST['profissao']."<br>";
+                echo "Pretensão Salarial: R$".$_POST['salario']."<br>";
+                echo "Experiências anteriores: ".($experiencia);
+                echo "<br>";?>
+            </p>
+
+            <h2 class="w3-border-bottom w3-padding-small">Desafios no Exercício Proposto:</h2>
+            <div class="w3-panel w3-border w3-padding w3-round-large">
+                <p class="w3-large w3-text-teal">PHP</p>
+                <ul class="w3-ul w3-margin-bottom">
+                    <li>Concatenação e na organização do código, por misturar html e php.</li>
+                    <li>Como verificar se no campo "sexo" tem marcado um "radio" ou tem algo escrito no campo "text".</li>
+                    <li>Como mostrar uma frase automática com "Sem experiência", caso a pessoa não escreva nada em "experiências anteriores".</li>
+                </ul>                
+                <p class="w3-large w3-text-teal">JavaScript</p>
+                <ul class="w3-ul">
+                    <li>Não tenho prática, tive que pesquisar no endereço: <a href="https://www.google.com/search?q=quero+que+no+html+quando+eu+clicar+no+input+text+ele+tira+a+sele%C3%A7%C3%A3o+do+radio+com+javascript&oq=quero+que+no+html+quando+eu+clicar+no+input+text+ele+tira+a+sele%C3%A7%C3%A3o+do+radio+com+javascript&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBCDQ5NDBqMGoxqAIAsAIA&sourceid=chrome&ie=UTF-8" target="_blank">clique aqui</a>, para tirar a seleção do campo "radio" caso o campo "text" tenha algo escrito.</li>
+                </ul>
+            </div>
+
+            <h2 class="w3-border-bottom w3-padding-small w3-margin-top">Organização dos dados no formulário:</h2>
+            <p>Eu pensei em como uma pessoa faria esse cadastro e quais os dados principais um recrutador precisaria. Porém, acho que faltou Estado e Cidade se fosse uma empresa nacional e não regional.</p>
+            <button class="w3-button w3-dark-gray w3-hover-gray w3-round-large w3-section" onclick="history.back()">Voltar</button>
+        </div>
+    </div>
+
+    <footer class="w3-container w3-center w3-margin-top w3-pale-blue">
+        <p>&copy; 2025 <a href="https://github.com/brendaHidalgos" class="w3-text-black">Brenda Hidalgo</a></p>
+    </footer>
+</body>
+</html>
